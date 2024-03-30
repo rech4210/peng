df19b66ac3b3ae7dd42fbd4dfce8756fd10978a9
rech4210 <rech0421k@gmail.com>
2024-03-30 11:30:04 +0900

    애니메이션 수정

---
+++ pengProject/Assets/Scripts/CommonScripts/DataContainer.cs
@@ -0,0 +1,30 @@
+namespace KMS.Enum
+{
+    public enum PenguinState
+    {
+        Pacing,
+        Working,
+        DozeOff,
+        Resting,
+        Eating
+    }
+
+}
+
+namespace KMS.Struct
+{
+    public struct Resource
+    {
+        public int bingneral;
+        public int energy;
+        public int emperorCoin;
+
+        public Resource(int bingneral, int energy, int emperorCoin)
+        {
+            this.bingneral = bingneral;
+            this.energy = energy;
+            this.emperorCoin = emperorCoin;
+        }
+    }
+}
+
